7ec1b324da17bfd6da032e9bef0d1f86f5611530
Beka Sturua <beqasturua3@gmail.com>
2022-09-02 23:27:36 +0400

    feat: test with useState

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
 const Greeting = () => {
+  const [changedText, setCHangedText] = useState(false);
+
+  const changedTextHandler = () => {
+    setCHangedText(true);
+  };
+
   return (
     <div>
       <h2>Hello World!</h2>
-      <p>It's good to see you!</p>
+      {!changedText && <p>It's good to see you!</p>}
+      {changedText && <p>Changed!</p>}
+      <button onClick={changedTextHandler}>Change Text!</button>
     </div>
   );
 };
